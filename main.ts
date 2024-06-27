b3d85fb5dfa727ab747b6da63be6b6d99d2c0b84
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-06-28 00:42:14 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    modules.ledStrip1.setAll(0x00ff00)
 })
+input.onButtonPressed(Button.B, function () {
+    modules.ledStrip1.setAll(0x000000)
+})
+modules.ledStrip1.configure(20, jacdac.LedStripLightType.WS2812B_GRB)
+modules.ledStrip1.setBrightness(10)

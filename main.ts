bb2a04afecf80079e5f1e5cdc47bf35faeedcfdd
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-07-02 18:17:55 +0100

    send to frank

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     modules.ledStrip1.setAll(0x00ff00)
 })
+input.onButtonPressed(Button.AB, function () {
+    modules.ledStrip1.setPixel(6, 0xff0000)
+})
 input.onButtonPressed(Button.B, function () {
     modules.ledStrip1.setAll(0x000000)
 })
-modules.ledStrip1.configure(20, jacdac.LedStripLightType.WS2812B_GRB)
+basic.showIcon(IconNames.Heart)
+modules.ledStrip1.configure(12, jacdac.LedStripLightType.WS2812B_GRB)
 modules.ledStrip1.setBrightness(10)
+basic.pause(500)
+modules.ledStrip1.setAll(0x000000)
